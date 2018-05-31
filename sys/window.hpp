7997d3b59f43e1e51f9d4c7cddc8eff9d5fa4c66
Brandon Walderman <bwalderman@outlook.com>
2018-05-30 18:25:04 -0700

    Convert tabs to spaces.

--- sys/window.hpp
+++ sys/window.hpp
@@ -9,25 +9,25 @@ namespace WebView
 class Window final
 {
 public:
-	Window(
-		const std::wstring& title,
-		const std::wstring& url,
-		SIZE size,
-		bool resizable);
-	~Window();
+    Window(
+        const std::wstring& title,
+        const std::wstring& url,
+        SIZE size,
+        bool resizable);
+    ~Window();
 
-	int Run() noexcept;
+    int Run() noexcept;
 
 private:
-	static LRESULT CALLBACK s_WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+    static LRESULT CALLBACK s_WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
-	winrt::Windows::Foundation::Rect _GetBounds();
-	void _UpdateBounds();
-	LRESULT _HandleMessage(UINT message, WPARAM wParam, LPARAM lParam);
+    winrt::Windows::Foundation::Rect _GetBounds();
+    void _UpdateBounds();
+    LRESULT _HandleMessage(UINT message, WPARAM wParam, LPARAM lParam);
 
-	HWND m_hwnd;
-	winrt::Windows::Web::UI::Interop::WebViewControlProcess m_process;
-	winrt::Windows::Web::UI::Interop::WebViewControl m_control = nullptr;
+    HWND m_hwnd;
+    winrt::Windows::Web::UI::Interop::WebViewControlProcess m_process;
+    winrt::Windows::Web::UI::Interop::WebViewControl m_control = nullptr;
 };
 
 }
